b8eace1261a7e12f6babd61e46118fc5e1312575
steffytw <steffythankamwilson@cs.ajce.in>
2020-02-27 17:59:29 +0530

    js programs

---
+++ cal1.js
@@ -0,0 +1,12 @@
+function mul(){
+            var x=document.getElementById("Num1").value;
+            var y=document.getElementById("Num2").value;
+            var z=x*y;
+            document.getElementById("res").value =z;
+        }
+        function div(){
+            var x=document.getElementById("Num1").value;
+            var y=document.getElementById("Num2").value;
+            var z=x/y;
+            document.getElementById("res").value =z;
+        }
